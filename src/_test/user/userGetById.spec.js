d71c872c7bdf42fdfec3c72f7e73b962250da662
SSSamvel <sssamvel75@gmail.com>
2024-08-15 21:41:00 -0400

    firstTest getbyId adjustments

--- src/_test/user/userGetById.spec.js
+++ src/_test/user/userGetById.spec.js
@@ -2,6 +2,7 @@ const { expect } = require('chai')
 const { userGetByIdQ } = require('./queries')
 const { gqlRequest } = require('../gqlRequest')
 const { userId } = require('./data')
+userId.userId = '66bad48621f088dc0cfb345f'
 
 let respData = null
 let postData = null
