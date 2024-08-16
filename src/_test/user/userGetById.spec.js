6fac62fc0c06f44a1ea11449997a399b80f2b0e7
SSSamvel <sssamvel75@gmail.com>
2024-08-15 23:03:25 -0400

    hooksAndHelpers 1

--- src/_test/user/userGetById.spec.js
+++ src/_test/user/userGetById.spec.js
@@ -1,18 +1,35 @@
 const { expect } = require('chai')
-const { userGetByIdQ } = require('./queries')
+const { userGetByIdQ, userCreateQ } = require('./queries')
 const { gqlRequest } = require('../gqlRequest')
-const { userId } = require('./data')
-userId.userId = '66bad48621f088dc0cfb345f'
+const { userId, user } = require('./data')
+
 
 let respData = null
 let postData = null
+let userID = null
 
 describe('getByID test', () => {
     describe('getByID test - positive', () => {
+        before('user create ', (done) => {
+            postData = {
+                query: userCreateQ,
+                variables: user
+            }
+            gqlRequest(postData)
+                .expect(200)
+                .end((err, res) => {
+                    if (err) return done (err)
+                    userID = res.body.data.userCreate._id
+                    console.log('userID =', userID)
+                    done()
+            })
+        }),
         it('user get by id', (done) => {
             postData = {
                 query: userGetByIdQ,
-                variables: userId
+                variables: {
+                    userId: userID
+                }
             }
             gqlRequest(postData)
                 .expect(200)
@@ -20,7 +37,7 @@ describe('getByID test', () => {
                     if (err) return done (err)
                     respData = res.body.data.userGetById
                     console.log(respData)
-                    expect(respData._id).eq('66bad48621f088dc0cfb345f')
+                    expect(respData._id).eq(userID)
                     done()
             })
         })
